fe1a999a89c6c4fa7a8f0aaf4aff311108598fdd
vini547 <carlos.cccoimbra@hotmail.com>
2019-06-14 12:40:17 -0300

    porra

--- esp8266code.ino
+++ esp8266code.ino
@@ -16,6 +16,38 @@ const char* password = "betina1802";
 
 ESP8266WebServer server(5555);
 
+String createJsonResponse() {
+  
+  StaticJsonBuffer<300> jsonBuffer;
+  JsonObject& object = jsonBuffer.createObject();
+  JsonObject& data = object.createNestedObject("data");
+   
+  JsonObject& accel = data.createNestedObject("accel");
+
+  JsonObject& gyro = data.createNestedObject("gyro");
+   
+  JsonObject& accelXl = accel.createNestedObject("accelX");
+  JsonObject& accelYl = accel.createNestedObject("accelY");
+  JsonObject& accelZl = accel.createNestedObject("accelZ");
+ 
+  JsonObject& gyroXl = gyro.createNestedObject("gyroX");
+  JsonObject& gyroYl = gyro.createNestedObject("gyroY");
+  JsonObject& gyroZl = gyro.createNestedObject("gyroZ");
+
+  object["nodeID"] = "mcu1";
+ 
+  accel["accelX"] = accelX;
+  accel["accelY"] = accelY;
+  accel["accelZ"] = accelZ;
+  gyro["gyroX"] = gyroX;
+  gyro["gyroY"] = gyroY;
+  gyro["gyroZ"] = gyroZ;
+
+  String json;
+  object.prettyPrintTo(json);
+  return json;
+}
+
 const int led = 2;
 
 
